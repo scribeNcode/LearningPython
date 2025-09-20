c6c9cc4781dfca185798cb2a09d8b49b09943616
Adeyeye Boluwatife <deyeye99@gmail.com>
2025-09-20 04:44:19 +0100

    day one tasks added

---
+++ day_1/helloworld.py
@@ -0,0 +1,53 @@
+print('python --version') # to print current python version
+print(3 + 4) #add 3 and 4
+print(3 - 4) # substract 4 from 3
+print(3 * 4) # multiply 3 by 4
+print(3 / 4) # divide 3 by 4
+print(3 ** 4) # exponential
+print(3 // 4) # floor division
+print('Boluwatife') # my first name
+print('Adeyeye') # my family name
+print('Nigeria') # my country
+print('I am enjoying 30 days of python')
+
+
+# Checking data types 
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Boluwatife'))
+print(type('Adeyeye'))
+print(type('Nigeria'))
+
+#examples of different data types
+
+#Number
+print(type(27))  # Integer
+print(type(3.5)) # Float
+print(type(4j)) # complex
+
+#String
+print (type('Adeyeye')) # String
+
+#Boolean
+print(type(True))
+
+#List
+print(type(['Adeyeye', 'Fullstack Dev', 27, 'True']))
+
+#Tuple
+print(type(('Boluwatife', 'Teacher', 27, True, [1,2,3,4] )))
+
+#Set
+print(type({2.5, 45, 'Ade'}))
+
+#Dictionary
+print(type(
+    {
+        'name' : 'Adeyeye Boluwatife',
+        'age': 27,
+        'gender' : 'Male'
+    }
+))
